33fb00760cae7ddffe2e57bb3ade597034dccbd6
growindiedev <rabhishekranjan@gmail.com>
2024-03-16 18:06:44 +0530

    remove localhost from wagmi chain

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
-import { localhost, polygonMumbai } from 'wagmi/chains';
+import { polygonMumbai } from 'wagmi/chains';
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
 export const config = getDefaultConfig({
   appName: 'DZap MT3 Staking',
   projectId: (process.env.NEXT_PUBLIC_RAINBOW_PROJECT_ID as string) || 'demo',
-  chains: [localhost, polygonMumbai],
+  chains: [polygonMumbai],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
